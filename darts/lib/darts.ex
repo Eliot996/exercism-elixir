c452e2d9fe303b6accd95c73cbf7e15893bbb651
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-15 23:14:14 +0100

    added solution

--- darts/lib/darts.ex
+++ darts/lib/darts.ex
@@ -6,5 +6,16 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance = distance_from_center(x, y)
+    cond do
+      distance <= 1  -> 10 # inner
+      distance <= 5  -> 5  # middel
+      distance <= 10 -> 1  # outer
+      distance >  10 -> 0  # outside
+    end
+  end
+
+  def distance_from_center(x, y) do 
+    :math.sqrt(:math.pow(x, 2) + :math.pow(y, 2))
   end
 end
