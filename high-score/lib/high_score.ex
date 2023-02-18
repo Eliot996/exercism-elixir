a586faef4d3dfb0507bb53c2e54db6c3e42a8f26
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-19 00:56:44 +0100

    added add_player and passes test

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,9 +1,8 @@
 defmodule HighScore do
   def new(), do: %{}
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
-  end
+  def add_player(scores, name), do: Map.put(scores, name, 0)
+  def add_player(scores, name, score), do: Map.put(scores, name, score)
 
   def remove_player(scores, name) do
     # Please implement the remove_player/2 function
