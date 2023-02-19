48e89f3ee9398debdc55c4238c66eec06858ba2e
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-19 01:18:31 +0100

    added comments

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,13 +1,19 @@
 defmodule HighScore do
+
+  @doc "Initializes a new empty map"
   def new(), do: %{}
 
+  @doc "Adds a new player to scores, default is 0, if not given a score"
   def add_player(scores, name), do: Map.put(scores, name, 0)
   def add_player(scores, name, score), do: Map.put(scores, name, score)
 
+  @doc "removes a player from the map"
   def remove_player(scores, name), do: Map.delete(scores, name)
 
+  @doc "sets the score of the name to 0"
   def reset_score(scores, name), do: Map.put(scores, name, 0)
 
+  @doc "updates the score of the name, but if the name already has a score then the score is added to the old"
   def update_score(scores, name, score) do 
     if scores[name] != nil do
       Map.put(scores, name, score + scores[name])
@@ -16,7 +22,7 @@ defmodule HighScore do
     end
   end
 
-  def get_players(scores) do
-    # Please implement the get_players/1 function
-  end
+  @doc "gets a list of all the player names in the given list"
+  def get_players(scores), do: Map.keys(scores)
+
 end
